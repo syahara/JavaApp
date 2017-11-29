b969871ec36e80fab8fc351c321b59313177f8b5
Syahara Utami Lekson <syahara.lekson.sl@gmail.com>
2017-11-29 12:00:38 +0700

    Delete SELASA 2.js

--- SELASA 2.js
+++
@@ -1,9 +0,0 @@
-console.log('Looping Pertama')
-for(var angka = 1; angka <= 20; angka++) {
-  console.log(angka + ' - I love coding')
-}
-
-console.log('Looping Pertama')
-for(var angka = 20; angka >= 1; angka--) {
-  console.log(angka + ' - I will become fullstack developer')
-}
